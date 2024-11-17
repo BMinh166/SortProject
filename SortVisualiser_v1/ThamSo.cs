338f3c35a60121e85f9bf06d64e2c25d80747bd8
tlee2110 <lephuocngoctan555@gmail.com>
2024-11-17 17:22:38 +0700

    update ucNode

---
+++ SortVisualiser_v1/ThamSo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortVisualiser_v1
+{
+    public static class ThamSo
+    {
+
+        public static int ChieuCaoDiChuyen = 40;
+        public static int KichCoNode = 46;
+        public static int KhoangCachCacNode = 20;
+        public static int SoluongNodeLonNhat = 15;
+        public static int ThoiGianDoi = 20;
+        public static int GiaTriLonNhat = 100;
+        public static int DoCaoDiChuyen = KichCoNode + 5;
+        public static Color mauNen = Color.White;
+        public static Color mauNodeHTSX = Color.Gray;
+        public static Color mauNodeDangSX = Color.AliceBlue;
+        public static Color Nodeketiep = Color.AntiqueWhite;
+    }
+}
--- SortVisualiser_v1/ucNode.Designer.cs
+++ SortVisualiser_v1/ucNode.Designer.cs
@@ -28,31 +28,53 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.tbx1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // button1
+            // btn1
             // 
-            this.button1.Location = new System.Drawing.Point(60, 55);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(115, 39);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btn1.Location = new System.Drawing.Point(51, 84);
+            this.btn1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(100, 28);
+            this.btn1.TabIndex = 0;
+            this.btn1.Text = "button1";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            this.btn1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.btn1_MouseDown);
             // 
-            // ucNode
+            // tbx1
+            // 
+            this.tbx1.Location = new System.Drawing.Point(34, 41);
+            this.tbx1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tbx1.Multiline = true;
+            this.tbx1.Name = "tbx1";
+            this.tbx1.Size = new System.Drawing.Size(132, 24);
+            this.tbx1.TabIndex = 1;
+            this.tbx1.TextChanged += new System.EventHandler(this.tbx1_TextChanged);
+            this.tbx1.Enter += new System.EventHandler(this.tbx1_Enter);
+            this.tbx1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbx1_KeyDown);
+            this.tbx1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbx1_KeyPress);
+            // 
+            // Node
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.button1);
-            this.Name = "ucNode";
-            this.Size = new System.Drawing.Size(238, 190);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.tbx1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Node";
+            this.Size = new System.Drawing.Size(200, 185);
+            this.Load += new System.EventHandler(this.TbxBtn_Load);
+            this.BackColorChanged += new System.EventHandler(this.TbxBtn_BackColorChanged);
             this.ResumeLayout(false);
-
+            this.PerformLayout();
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.TextBox tbx1;
     }
 }
